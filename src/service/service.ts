5d05ab118bb11809626c8a2ac8dc907ce32f5208
shunn2 <chsida655@gmail.com>
2024-06-03 03:44:16 +0900

    feat: api interceptor & middleware 초안

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,3 +1,5 @@
+import { requestInterceptors, responseInterceptors, runInterceptors } from './interceptor';
+
 interface HTTPInstance {
   get<T>(url: string, config?: RequestInit): Promise<T>;
   delete<T>(url: string, config?: RequestInit): Promise<T>;
@@ -37,26 +39,37 @@ class Service {
     method: string,
     url: string,
     data?: unknown,
-    config?: RequestInit
+    config: RequestInit = {}
   ): Promise<T> {
+    const requestConfig: RequestInit & { url: string; method: string } = {
+      ...config,
+      method,
+      headers: {
+        ...this.headers,
+        'Content-Type': 'application/json',
+        ...config.headers
+      },
+      credentials: 'include',
+      body: data ? JSON.stringify(data) : undefined,
+      url: this.baseURL + url
+    };
+
     try {
-      const response = await fetch(this.baseURL + url, {
-        method,
-        headers: {
-          ...this.headers,
-          'Content-Type': 'application/json',
-          ...config?.headers
-        },
-        credentials: 'include',
-        body: data ? JSON.stringify(data) : undefined,
-        ...config
-      });
+      await runInterceptors(requestInterceptors, requestConfig);
+
+      const { url: requestUrl, ...fetchConfig } = requestConfig;
+      const response = await fetch(requestUrl, fetchConfig);
 
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
 
       const responseData: T = await response.json();
+
+      await runInterceptors(responseInterceptors, {
+        ...requestConfig,
+        response: responseData
+      } as RequestInit & { url: string; method: string; response?: unknown });
 
       return responseData;
     } catch (error) {
@@ -65,31 +78,31 @@ class Service {
     }
   }
 
-  private get<T>(url: string, config?: RequestInit): Promise<T> {
+  private get<T>(url: string, config: RequestInit = {}): Promise<T> {
     return this.request<T>('GET', url, undefined, config);
   }
 
-  private delete<T>(url: string, config?: RequestInit): Promise<T> {
+  private delete<T>(url: string, config: RequestInit = {}): Promise<T> {
     return this.request<T>('DELETE', url, undefined, config);
   }
 
-  private head<T>(url: string, config?: RequestInit): Promise<T> {
+  private head<T>(url: string, config: RequestInit = {}): Promise<T> {
     return this.request<T>('HEAD', url, undefined, config);
   }
 
-  private options<T>(url: string, config?: RequestInit): Promise<T> {
+  private options<T>(url: string, config: RequestInit = {}): Promise<T> {
     return this.request<T>('OPTIONS', url, undefined, config);
   }
 
-  private post<T>(url: string, data?: unknown, config?: RequestInit): Promise<T> {
+  private post<T>(url: string, data?: unknown, config: RequestInit = {}): Promise<T> {
     return this.request<T>('POST', url, data, config);
   }
 
-  private put<T>(url: string, data?: unknown, config?: RequestInit): Promise<T> {
+  private put<T>(url: string, data?: unknown, config: RequestInit = {}): Promise<T> {
     return this.request<T>('PUT', url, data, config);
   }
 
-  private patch<T>(url: string, data?: unknown, config?: RequestInit): Promise<T> {
+  private patch<T>(url: string, data?: unknown, config: RequestInit = {}): Promise<T> {
     return this.request<T>('PATCH', url, data, config);
   }
 }
