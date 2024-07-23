3927cba2aba6990cd07e7980cd1f8e8cb58e975d
shunn2 <chsida655@gmail.com>
2024-07-23 17:41:13 +0900

    feat: 공통 이미지 컴포넌트 초안

---
+++ src/components/common/Image.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import React, { useState } from 'react';
+
+import styles from './CommonImage.module.css';
+
+interface ImageProps {
+  src: string;
+  alt?: string;
+  width?: number;
+  height?: number;
+  onClick?: () => void;
+}
+
+/**
+ * parent position relative 추천
+ * width & height 없으면 fill
+ */
+const CommonImage = (props: ImageProps) => {
+  const { src, alt = 'Loading', width, height, onClick = () => {} } = props;
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoad = () => {
+    setIsLoading(false);
+  };
+
+  return (
+    <div className={styles.imageContainer}>
+      {isLoading && <div>Skeleton!</div>}
+      <Image
+        src={src}
+        alt={alt}
+        fill={!width && !height}
+        width={width}
+        height={height}
+        onLoad={handleLoad}
+        onClick={onClick}
+        className={isLoading ? styles.hidden : ''}
+      />
+    </div>
+  );
+};
+
+export default CommonImage;
