826db6ec54380dfd25947ad165426f119d4cae71
shunn2 <chsida655@gmail.com>
2024-08-18 21:50:23 +0900

    feat: tailwind css color theme 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,35 @@ module.exports = {
   content: ['./src/app/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      fontFamily: {
-        primary: ['var(--font-primary)']
-      }
-    }
+      colors: {
+        slate: {
+          50: '#f8fafc',
+          100: '#f1f5f9',
+          200: '#e2e8f0',
+          300: '#cbd5e1',
+          400: '#94a3b8',
+          500: '#64748b',
+          600: '#475569',
+          700: '#334155',
+          800: '#1e293b',
+          900: '#0f172a',
+          950: '#040c15',
+        },
+        green: {
+          50: '#f0fdf4',
+          100: '#dcfce7',
+          200: '#bef3c9',
+          300: '#86efac',
+          400: '#4ade80',
+          500: '#22c55e',
+          600: '#16a34a',
+          700: '#066f38',
+          800: '#044b29',
+          900: '#03251a',
+          950: '#01160f',
+        },
+      },
+    },
   },
   variants: {
     extend: {}
