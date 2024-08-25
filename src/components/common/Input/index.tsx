41f9f2fefd68b2271bbd891f741becc7fbdcd4ef
shunn2 <chsida655@gmail.com>
2024-08-25 23:32:18 +0900

    fix: 디자인 시스템 focus 색상 변경

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -18,7 +18,7 @@ const SInput = ({ className = '', placeholder, value, onChange, type = 'text' }:
   return (
     <input
       type={type}
-      className={`w-full px-4 py-2 border border-slate-300 rounded-lg bg-white focus:outline-none focus:ring-2 focus:ring-blue-300 ${className}`}
+      className={`w-full px-4 py-2 border border-slate-300 rounded-lg bg-white focus:outline-none focus:ring-2 focus:ring-tree-300 ${className}`}
       placeholder={placeholder}
       value={value}
       onChange={handleChange}
