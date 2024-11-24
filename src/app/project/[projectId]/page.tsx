2d9af50e8637075e1768a48f70fa6e7be658b663
shunn2 <chsida655@gmail.com>
2024-11-25 02:23:45 +0900

    feat: swiper 공통 컴포넌트 초안

--- src/app/project/[projectId]/page.tsx
+++ src/app/project/[projectId]/page.tsx
@@ -2,6 +2,10 @@ import { getDehydratedQuery, Hydrate } from '@/hooks/react-query/react-query';
 import ProjectQueryOptions from '@/service/project/queries';
 import { redirect } from 'next/navigation';
 
+import SImage from '@/components/common/Image';
+import RouterPush from '@/components/custom/RouterPush';
+import SwiperComponent from '@/components/common/Swiper';
+
 interface ProjectDetailPageProps {
   params: {
     projectId: string;
@@ -15,13 +19,27 @@ const ProjectDetailPage = async ({ params }: ProjectDetailPageProps) => {
 
   const query = await getDehydratedQuery({ queryKey, queryFn });
 
+  const data = query.state.data;
+
+  const sampleImages = new Array(10).fill('https://picsum.photos/600/400');
+
+  const sampleImageComponent = sampleImages.map((src, index) => (
+    <SImage key={index} src={src} width={400} height={30} alt={`Slide ${index + 1}`} />
+  ));
+
   if (!query) {
     redirect('/404');
   }
 
   return (
-    <div className="p-10 bg-slate-95">
-      <Hydrate state={{ queries: [query] }}></Hydrate>
+    <div className="px-20 pt-10 pb-20 bg-slate-95">
+      <Hydrate state={{ queries: [query] }}>
+        <RouterPush className="px-2 py-1.5 flex items-center gap-1 cursor-pointer" path="/">
+          <SImage src="/leftArrow.svg" width={14} height={14} />
+          <p className="text-small text-slate-30">프로젝트 목록</p>
+        </RouterPush>
+        <SwiperComponent items={sampleImageComponent} />
+      </Hydrate>
     </div>
   );
 };
---
+++ src/components/common/Swiper/index.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import React, { useEffect, ReactNode, useRef, useState } from 'react';
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import '@/styles/swiper.css';
+
+interface SwiperComponentProps {
+  items: ReactNode[]; // 이미지나 children을 전달받기 위한 배열
+}
+
+const SwiperComponent: React.FC<SwiperComponentProps> = ({ items }) => {
+  const swiperRef = useRef<SwiperRef | null>(null); // Swiper 인스턴스를 참조할 ref
+  const [activeIndex, setActiveIndex] = useState(0); // 활성 슬라이드의 인덱스 상태 관리
+
+  useEffect(() => {
+    if (swiperRef.current) {
+      swiperRef.current.swiper.update();
+    }
+  }, [items]);
+
+  return (
+    <div className="relative w-full">
+      <Swiper
+        ref={swiperRef} // Swiper 인스턴스를 참조
+        modules={[Navigation]}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }}
+        spaceBetween={20}
+        centeredSlides={true} // 동적으로 centeredSlides 값 적용
+        centeredSlidesBounds={true}
+        breakpoints={{
+          480: { slidesPerView: 2 },
+          768: { slidesPerView: 3 },
+          1024: { slidesPerView: 5 }
+        }}
+        onSlideChange={(swiper) => {
+          setActiveIndex(swiper.activeIndex);
+
+          const prevButton = document.querySelector('.swiper-button-prev');
+          const nextButton = document.querySelector('.swiper-button-next');
+
+          // 첫 번째 슬라이드에서 넘어갈 수 없도록 설정
+          if (swiper.isBeginning) {
+            (prevButton as HTMLElement).style.display = 'none'; // 첫 번째 슬라이드에서 이전 버튼 숨기기
+          } else {
+            (prevButton as HTMLElement).style.display = 'block';
+          }
+
+          // 마지막 슬라이드에서 넘어갈 수 없도록 설정
+          if (swiper.isEnd) {
+            (nextButton as HTMLElement).style.display = 'none'; // 마지막 슬라이드에서 다음 버튼 숨기기
+          } else {
+            (nextButton as HTMLElement).style.display = 'block';
+          }
+        }}
+      >
+        {items.map((item, index) => (
+          <SwiperSlide
+            key={index}
+            className="swiper-slide flex items-center justify-center transition-transform duration-300 scale-75 active:scale-100"
+          >
+            {item}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      {/* Navigation Buttons */}
+      <button className="swiper-button-prev absolute left-0 z-10 hidden bg-black p-2 text-white"></button>
+      <button className="swiper-button-next absolute right-0 z-10 hidden bg-black p-2 text-white"></button>
+    </div>
+  );
+};
+
+export default SwiperComponent;
