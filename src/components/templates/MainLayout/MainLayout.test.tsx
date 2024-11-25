07f09bf48a53c17a1b5b57f08a74a98f51e6122c
yunko <yk1803@nyu.edu>
2024-11-25 18:06:00 +0900

    chore: pass the test

--- src/components/templates/MainLayout/MainLayout.test.tsx
+++ src/components/templates/MainLayout/MainLayout.test.tsx
@@ -1,13 +1,17 @@
 import { render, screen } from '@/tests/test-utils';
 
+import SessionWrapper from '@/components/SessionWrapper';
+
 import { MainLayout } from '.';
 
 describe('MainLayout', () => {
   it('should render the children components', () => {
     render(
-      <MainLayout>
-        <h1>MainLayout children</h1>
-      </MainLayout>
+      <SessionWrapper>
+        <MainLayout>
+          <h1>MainLayout children</h1>
+        </MainLayout>
+      </SessionWrapper>
     );
 
     // Assert
