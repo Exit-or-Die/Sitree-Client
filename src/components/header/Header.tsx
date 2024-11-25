4342c34787fbb781c02b3a976bf87fb8f2ffee62
yunko <yk1803@nyu.edu>
2024-11-25 17:50:47 +0900

    feat: update header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,14 @@
 'use client';
 
-import WithModal from '@/enhancers/WithModal';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React, { useState } from 'react';
-
-import SignInModal from '@/components/account/SignInModal';
+import WithModal from '@/enhancers/WithModal';
 
 import SButton from '../common/Button';
-import SImage from '../common/Image';
 import SInput from '../common/Input';
+import SignInModal from '@/components/account/SignInModal';
+import SImage from '../common/Image';
 
 export const Header = () => {
   const SignWithModal = WithModal(SignInModal);
@@ -39,15 +38,14 @@ export const Header = () => {
         </nav>
       </div>
 
-      {!session ? (
-        <SButton
+      {!session ? 
+        (<SButton 
           className="text-sm h-[36px] text-slate-40 border border-slate-90 rounded-base hover:bg-slate-100 transition-all"
           onClick={() => setToggleLogin(true)}
         >
           로그인
-        </SButton>
-      ) : (
-        <div className="flex items-center space-x-4">
+        </SButton>) : 
+        (<div className="flex items-center space-x-4">
           <div className="relative flex items-center w-[200px] h-[36px] bg-slate-100 rounded-base text-small pl-3">
             <SImage
               src="/magnifyGlass.svg"
@@ -62,7 +60,7 @@ export const Header = () => {
               className="bg-transparent text-slate-40 w-full placeholder-slate-60 border-none focus:ring-0"
             />
           </div>
-
+  
           <SButton className="flex items-center px-4 w-[120px] h-[36px] text-sm font-medium text-tree-30 bg-tree-93 border-none hover:bg-green-200 rounded-base">
             <SImage
               src="/write.svg"
@@ -73,7 +71,7 @@ export const Header = () => {
             />
             새 프로덕트
           </SButton>
-
+  
           <div className="flex items-center space-x-2">
             <div className="w-8 h-8 rounded-full bg-slate-95 flex items-center justify-center">
               <SImage
@@ -86,8 +84,8 @@ export const Header = () => {
             </div>
             <span className="text-slate-40 text-small font-md">{session?.user?.name}</span>
           </div>
-        </div>
-      )}
+        </div>)
+      }
     </header>
   );
 };
