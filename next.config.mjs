6628a6b023e10d3009a8cefe76f71e15d2a2c508
yunko <yk1803@nyu.edu>
2025-01-31 13:27:28 +0900

    feat: create rough design of sitree pick component

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: '2.img-dpreview.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 't4.ftcdn.net'
       }
     ]
   }
--- src/components/sitreePick/SitreePick.tsx
+++ src/components/sitreePick/SitreePick.tsx
@@ -1,104 +1,128 @@
 'use client';
 
 import Image from 'next/image';
-import React, { useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import React, { useState, useRef, useEffect } from 'react';
 
 const mockProjects = [
   {
     id: 1,
-    name: 'Project Name',
+    name: '10분만',
+    icon: 'https://t4.ftcdn.net/jpg/01/43/42/83/360_F_143428338_gcxw3Jcd0tJpkvvb53pfEztwtU9sxsgT.jpg',
+    views: '42.9k',
+    likes: '78.7k',
+    comments: '9.64k',
     images: [
-      'https://2.img-dpreview.com/files/p/E~C1000x0S4000x4000T1200x1200~articles/3925134721/0266554465.jpeg',
-      'https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg'
+      'https://2.img-dpreview.com/files/p/E~C1000x0S4000x4000T1200x1200~articles/3925134721/0266554465.jpeg'
     ]
   },
   {
     id: 2,
-    name: 'STEPIT',
+    name: 'Moring',
+    icon: 'https://t4.ftcdn.net/jpg/01/43/42/83/360_F_143428338_gcxw3Jcd0tJpkvvb53pfEztwtU9sxsgT.jpg',
+    views: '42.9k',
+    likes: '32.8k',
+    comments: '1.72k',
     images: [
-      'https://2.img-dpreview.com/files/p/E~C1000x0S4000x4000T1200x1200~articles/3925134721/0266554465.jpeg',
-      'https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg'
+      'https://t4.ftcdn.net/jpg/01/43/42/83/360_F_143428338_gcxw3Jcd0tJpkvvb53pfEztwtU9sxsgT.jpg'
     ]
   },
   {
     id: 3,
-    name: 'Trouble Painter',
+    name: 'SPOT!',
+    icon: 'https://t4.ftcdn.net/jpg/01/43/42/83/360_F_143428338_gcxw3Jcd0tJpkvvb53pfEztwtU9sxsgT.jpg',
+    views: '23.7k',
+    likes: '98.2k',
+    comments: '482',
     images: [
-      'https://2.img-dpreview.com/files/p/E~C1000x0S4000x4000T1200x1200~articles/3925134721/0266554465.jpeg',
-      'https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg'
+      'https://t4.ftcdn.net/jpg/01/43/42/83/360_F_143428338_gcxw3Jcd0tJpkvvb53pfEztwtU9sxsgT.jpg'
     ]
   },
   {
     id: 4,
     name: '일이삼사오육칠팔구십일...',
+    icon: 'https://t4.ftcdn.net/jpg/01/43/42/83/360_F_143428338_gcxw3Jcd0tJpkvvb53pfEztwtU9sxsgT.jpg',
+    views: '18.4k',
+    likes: '67.2k',
+    comments: '623',
     images: [
-      'https://2.img-dpreview.com/files/p/E~C1000x0S4000x4000T1200x1200~articles/3925134721/0266554465.jpeg',
-      'https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg'
+      'https://t4.ftcdn.net/jpg/01/43/42/83/360_F_143428338_gcxw3Jcd0tJpkvvb53pfEztwtU9sxsgT.jpg'
     ]
   },
   {
     id: 5,
     name: '개미는툰툰',
+    icon: 'https://t4.ftcdn.net/jpg/01/43/42/83/360_F_143428338_gcxw3Jcd0tJpkvvb53pfEztwtU9sxsgT.jpg',
+    views: '13.1k',
+    likes: '39.1k',
+    comments: '397',
     images: [
-      'https://2.img-dpreview.com/files/p/E~C1000x0S4000x4000T1200x1200~articles/3925134721/0266554465.jpeg',
-      'https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg'
+      'https://t4.ftcdn.net/jpg/01/43/42/83/360_F_143428338_gcxw3Jcd0tJpkvvb53pfEztwtU9sxsgT.jpg'
     ]
   }
 ];
 
 const SitreePick = () => {
   const [selectedProject, setSelectedProject] = useState(mockProjects[0]);
+  const selectedRef = useRef(null);
+  const [position, setPosition] = useState(0);
+
+  useEffect(() => {
+    if (selectedRef.current) {
+      setPosition(selectedRef.current.offsetTop);
+    }
+  }, [selectedProject]);
 
   return (
     <div className="bg-white rounded-xl p-6 w-[954px]">
-      <h2 className="text-lg font-bold">사이트리 PICK</h2>
-      <div className="flex shadow">
-        <div className="w-1/3 space-y-4 shadow">
-          <ul className="space-y-4">
-            {mockProjects.map((project, index) => (
+      <h2 className="text-lg font-bold mb-4">사이트리 PICK</h2>
+      <div className="flex shadow-lg border rounded-lg">
+        <div className="relative w-1/3 space-y-2 pl-2">
+          <div
+            className="absolute left-2 w-[95%] h-[60px] border bg-gray-100 rounded-lg shadow-lg transition-all duration-300"
+            style={{ top: `${position}px` }}
+          />
+          <ul>
+            {mockProjects.map((project) => (
               <li
                 key={project.id}
-                className={`flex items-center p-2 cursor-pointer rounded-lg ${
-                  selectedProject.id === project.id ? 'bg-gray-200' : ''
-                }`}
+                ref={selectedProject.id === project.id ? selectedRef : null}
+                className="relative flex items-center p-3 cursor-pointer rounded-lg transition"
                 onClick={() => setSelectedProject(project)}
               >
-                <div className="text-gray-500 text-sm w-8">{index + 1}</div>
-                <div className="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center overflow-hidden">
-                  {index === 1 ? (
-                    <Image
-                      src="/select.svg"
-                      alt="Project Thumbnail"
-                      width={40}
-                      height={40}
-                      className="object-cover"
-                    />
-                  ) : null}
+                <div className="w-10 h-10 rounded-lg overflow-hidden">
+                  <Image
+                    src={project.icon}
+                    alt={`${project.name} Icon`}
+                    width={40}
+                    height={40}
+                    className="object-cover"
+                  />
                 </div>
                 <div className="ml-4 flex flex-col">
                   <span className="text-sm font-medium truncate">{project.name}</span>
-                  <span className="text-xs text-gray-400">♥ 000 조회수 000</span>
+                  <span className="text-xs text-gray-400 flex gap-2">
+                    <span>
+                      <Image src="/comment.svg" width={10} height={10} alt="comment" />{' '}
+                      {project.comments}
+                    </span>
+                    <span>
+                      <Image src="/like.svg" width={10} height={10} alt="like" /> {project.likes}
+                    </span>
+                    <span>조회수 {project.views}</span>
+                  </span>
                 </div>
               </li>
             ))}
           </ul>
         </div>
-        <div className="w-2/3 flex items-center justify-center">
+        <div className="w-2/3 flex items-center justify-center relative">
           {selectedProject.images.length > 0 ? (
-            <Swiper spaceBetween={10} slidesPerView={1} className="w-full h-full">
-              {selectedProject.images.map((image, index) => (
-                <SwiperSlide key={index}>
-                  <Image
-                    src={image}
-                    alt={`Project ${selectedProject.name} Image ${index + 1}`}
-                    layout="fill"
-                    className="rounded-lg object-cover"
-                  />
-                </SwiperSlide>
-              ))}
-            </Swiper>
+            <Image
+              src={selectedProject.images[0]}
+              alt={`Project ${selectedProject.name} Background`}
+              layout="fill"
+              className="rounded-r-lg object-cover"
+            />
           ) : (
             <div className="text-gray-500">No images available</div>
           )}
