8b6277027a36e91661929a6a8db28e90145fd9bf
shunn2 <chsida655@gmail.com>
2025-02-03 01:52:45 +0900

    feat: SInput 내부 아이콘 삽입 가능하도록 구조 변경 및 이벤트 부착

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -3,6 +3,8 @@
 import React from 'react';
 import { FieldValues, UseFormRegister } from 'react-hook-form';
 
+import SImage from '../Image';
+
 interface InputProps<T extends FieldValues> {
   className?: string;
   placeholder?: string;
@@ -12,12 +14,27 @@ interface InputProps<T extends FieldValues> {
   register?: UseFormRegister<T>;
   name?: string;
   accept?: string;
+  iconName?: string; // 아이콘 이름을 받는 props 추가
+  onEnterPress?: () => void; // 엔터 키 눌렀을 때 실행할 함수 추가
+  onIconClick?: () => void; // 아이콘 클릭 시 실행할 함수 추가
 }
 
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 const SInput = React.forwardRef<HTMLInputElement, InputProps<any>>(
   (
-    { className = '', placeholder, value, onChange, name, register, type = 'text', accept },
+    {
+      className = '',
+      placeholder,
+      value,
+      onChange,
+      name,
+      register,
+      type = 'text',
+      accept,
+      iconName,
+      onEnterPress,
+      onIconClick
+    },
     ref
   ) => {
     const {
@@ -34,17 +51,44 @@ const SInput = React.forwardRef<HTMLInputElement, InputProps<any>>(
       });
     };
 
+    // 엔터 키 눌렀을 때 처리
+    const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter' && onEnterPress) {
+        onEnterPress();
+      }
+    };
+
+    // 아이콘 클릭 이벤트 처리
+    const handleIconClick = () => {
+      if (onIconClick) {
+        onIconClick();
+      }
+    };
+
     return (
-      <input
-        type={type}
-        ref={registerRef || ref}
-        className={`w-full p-3 border border-slate-300 rounded-base bg-white focus:outline-none focus:ring-2 focus:ring-tree-300 ${className}`}
-        placeholder={placeholder}
-        value={value}
-        onChange={handleChange}
-        accept={accept}
-        {...restRegister}
-      />
+      <div className={`relative`}>
+        <input
+          type={type}
+          ref={registerRef || ref}
+          className={`w-full p-3 pr-10 border border-slate-300 rounded-base bg-white focus:outline-none focus:ring-2 focus:ring-tree-300 ${className}`}
+          placeholder={placeholder}
+          value={value}
+          onChange={handleChange}
+          onKeyPress={handleKeyPress} // 엔터 키 이벤트 추가
+          accept={accept}
+          {...restRegister}
+        />
+        {iconName && (
+          <SImage
+            src={`/${iconName}.svg`} // 아이콘 파일 경로 예시
+            alt={iconName}
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+            width={24}
+            height={24}
+            onClick={handleIconClick} // 아이콘 클릭 이벤트 추가
+          />
+        )}
+      </div>
     );
   }
 );
