67da630d3b9d4057aac51a45822d58a931b73e6b
shunn2 <chsida655@gmail.com>
2025-02-12 18:44:47 +0900

    chore: 프로젝트 등록 관련 파일 삭제

--- src/components/project/register/section/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ProjectRegisterHead } from './head';
-export { default as ProjectRegisterOverview } from './overview';
-export { default as ProjectRegisterParticipantList } from './participantList';
-export { default as ProjectRegisterTechViewList } from './techViewList';
