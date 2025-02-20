28060ac4f2b78b1f997ff5fa17cf0af2d28c3ad1
shunn2 <chsida655@gmail.com>
2025-02-20 23:42:17 +0900

    feat: 코멘트 컴포넌트 initialData 중복 렌더링 수정

--- src/components/project/comment/index.tsx
+++ src/components/project/comment/index.tsx
@@ -3,6 +3,7 @@
 import useInfiniteScroll from '@/hooks/infiniteScroll/useInfiniteScroll';
 import CommentsQueryOptions from '@/service/comments/queries';
 import { GetCommentListResponse } from '@/service/comments/response';
+import { Participant } from '@/service/project/response';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { useRef } from 'react';
 import { Nullable } from 'types/common';
@@ -13,26 +14,30 @@ import CommentListComponent from './CommentListComponent';
 interface CommentComponentProps {
   commentInfo?: GetCommentListResponse;
   projectId: string;
+  teamMember: Array<Participant>;
 }
 
-const CommentComponent = ({ commentInfo, projectId }: CommentComponentProps) => {
+const CommentComponent = ({ commentInfo, projectId, teamMember }: CommentComponentProps) => {
   const { queryKey, queryFn } = CommentsQueryOptions.retrieveCommentList(projectId);
 
   const { fetchNextPage, hasNextPage, isFetchingNextPage, data } = useInfiniteQuery({
     queryKey: [...queryKey, 'infinite'],
     queryFn: ({ pageParam }) => queryFn({ pageParam }),
-    initialPageParam: 0,
+    initialPageParam: 1,
     getNextPageParam: (lastPage, allPages) => {
-      return lastPage?.hasNext ? allPages.length : undefined;
+      return lastPage?.hasNext ? allPages.length + 1 : undefined;
     },
     select: (data) => ({
       comments: data.pages.flatMap((page) => page?.content ?? []),
       total: data.pages[0]?.total ?? 0
     }),
-    initialData: {
-      pages: [commentInfo],
-      pageParams: [0]
-    }
+    initialData: commentInfo
+      ? {
+          pages: [commentInfo],
+          pageParams: [1]
+        }
+      : undefined,
+    enabled: !!commentInfo
   });
 
   const triggerRef = useRef<Nullable<HTMLDivElement>>(null);
@@ -40,7 +45,7 @@ const CommentComponent = ({ commentInfo, projectId }: CommentComponentProps) => 
   useInfiniteScroll({
     ref: triggerRef,
     onScrollEnd: async () => {
-      if (hasNextPage) {
+      if (hasNextPage && !isFetchingNextPage) {
         await fetchNextPage();
       }
     },
@@ -51,7 +56,7 @@ const CommentComponent = ({ commentInfo, projectId }: CommentComponentProps) => 
   return (
     <div className="bg-white-100 rounded-2xlarge p-10 border-[1px] border-slate-90">
       <CommentHeaderComponent totalCount={data?.total || 0} />
-      <CommentListComponent commentList={data?.comments || []} />
+      <CommentListComponent commentList={data?.comments || []} teamMember={teamMember} />
       {hasNextPage && <div ref={triggerRef} className="h-10" />}
     </div>
   );
