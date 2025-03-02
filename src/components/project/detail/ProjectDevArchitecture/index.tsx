2248e1a6acf76d272ff10a28ceb97f5379d03e0d
shunn2 <chsida655@gmail.com>
2025-03-03 01:37:11 +0900

    feat: 아키텍쳐 이미지 높이에 맞춰 노출 default 일때는 높이 고정

--- src/components/project/detail/ProjectDevArchitecture/index.tsx
+++ src/components/project/detail/ProjectDevArchitecture/index.tsx
@@ -2,7 +2,7 @@
 
 import { Architecture } from '@/service/project/response';
 import dynamic from 'next/dynamic';
-import { useState } from 'react';
+import { use, useEffect, useState } from 'react';
 
 import SImage from '@/components/common/Image';
 import STab from '@/components/common/Tab';
@@ -15,6 +15,7 @@ interface ProjectDevArchitectureProps {
 
 const ProjectDevArchitecture = ({ architectureList }: ProjectDevArchitectureProps) => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [isDefaultImage, setIsDefaultImage] = useState<boolean>(false); // Default 이미지 여부 상태 추가
 
   if (architectureList.length === 0) {
     return null;
@@ -27,13 +28,21 @@ const ProjectDevArchitecture = ({ architectureList }: ProjectDevArchitectureProp
         items={architectureList.map((architecture) => architecture.architectureType)}
         onChange={(item, index) => setCurrentIndex(index)}
       />
-      <div className="relative w-full aspect-[16/9] overflow-hidden rounded-large border border-1 border-slate-90">
+      <div
+        className={`relative w-full flex justify-center items-center rounded-large border border-1 border-slate-90 bg-black ${
+          isDefaultImage ? 'h-[260px]' : 'aspect-[16/9] max-h-[500px]'
+        }`}
+      >
         <SImage
           src={architectureList[currentIndex].architectureImage.imageUrl ?? ''}
-          className="object-cover"
+          width={0}
+          height={isDefaultImage ? 260 : 0}
+          className={`object-contain ${isDefaultImage ? 'w-full h-full object-cover' : ''}`}
           alt="Sitree Architecture Image"
+          onDefaultImageLoad={setIsDefaultImage}
         />
       </div>
+
       <SViewer content={architectureList[currentIndex].architectureDesc} key={currentIndex} />
     </div>
   );
