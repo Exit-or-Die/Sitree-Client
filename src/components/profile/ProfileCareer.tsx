64a0b0bcb97f4256317d77b36b9fe9813d2daab2
yunko <yk1803@nyu.edu>
2025-03-12 15:37:44 +0900

    feat: complete markup for profile career component

--- src/components/profile/ProfileCareer.tsx
+++ src/components/profile/ProfileCareer.tsx
@@ -16,17 +16,21 @@ const ProfileCareer = ({ careers }: Props) => {
       <div className="flex items-start mt-[20px] mb-6">
         <div className="w-12 h-12 bg-gray-200 rounded-md"></div>
         <div className="ml-4">
-          <h3 className="text-lg font-bold">마이크로소프트</h3>
-          <p className="text-gray-500 text-sm">프로덕트디자이너(애는 직접 입력) | dd</p>
-          <p className="text-gray-400 text-xs">YYYY.MM ~ YYYY.MM</p>
+          <h3 className="text-large font-lg">마이크로소프트</h3>
+          <div className="flex items-center">
+            <div className="text-small text-slate-30 font-lg">프로덕트디자이너(애는 직접 입력)</div>
+            <div className="border-l-2 mx-[10px] h-[14px]" />
+            <div className="text-small text-slate-50 font-md">Team</div>
+          </div>
+          <p className="text-slate-30 text-xsmall">YYYY.MM ~ YYYY.MM</p>
         </div>
       </div>
 
       {[1, 2].map((project, index) => (
         <div key={index} className="mb-6 border-l-2 pt-[12px] ml-[70px] pl-[20px]">
-          <h4 className="text-md font-semibold">권한관리시스템 UXUI 개선</h4>
-          <p className="text-gray-400 text-xs">YYYY.MM ~ YYYY.MM</p>
-          <p className="text-gray-600 mt-2">
+          <h4 className="text-[15px] font-semibold text-slate-10">권한관리시스템 UXUI 개선</h4>
+          <p className="text-slate-50 text-xsmall">YYYY.MM ~ YYYY.MM</p>
+          <p className="text-slate-30 text-small mt-2">
             {index === 0 ? (
               <>
                 어쩌고 저쩌고 엄청나게 많은 것을 했습니다.
@@ -44,7 +48,7 @@ const ProfileCareer = ({ careers }: Props) => {
             {['PM/PO', 'PD', 'FE', 'BE', 'INFRA', 'AI', 'DATA'].map((tag) => (
               <span
                 key={tag}
-                className="px-3 py-1 bg-gray-100 text-gray-600 text-xs rounded-full"
+                className="px-3 py-1 bg-gray-100 text-slate-20 text-[13px] rounded-large"
               >
                 {tag}
               </span>
