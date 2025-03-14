ef19ebc1e103226ff1cd829fd2a1209eb7bbf53a
shunn2 <chsida655@gmail.com>
2025-03-14 20:46:03 +0900

    feat: 프로젝트 상세 별 메타데이터 주입

--- src/app/project/[projectId]/page.tsx
+++ src/app/project/[projectId]/page.tsx
@@ -1,4 +1,4 @@
-import { getDehydratedQueries, Hydrate } from '@/hooks/react-query/react-query';
+import { getDehydratedQueries, getDehydratedQuery, Hydrate } from '@/hooks/react-query/react-query';
 import CommentsQueryOptions from '@/service/comments/queries';
 import { GetCommentListResponse } from '@/service/comments/response';
 import ProjectQueryOptions from '@/service/project/queries';
@@ -15,6 +15,58 @@ import ProjectDetailSideBar from '@/components/project/detail/ProjectDetailSideB
 interface ProjectDetailPageProps {
   params: {
     projectId: string;
+  };
+}
+
+export async function generateMetadata({ params }: ProjectDetailPageProps) {
+  const { queryKey: projectDetailKey, queryFn: projectDetailFn } =
+    ProjectQueryOptions.retrieveProjectDetail(params.projectId);
+
+  const projectDetailQuery = await getDehydratedQuery({
+    queryKey: projectDetailKey,
+    queryFn: projectDetailFn
+  });
+
+  const projectDetail = projectDetailQuery?.state.data as ProjectDetailResponse;
+
+  if (!projectDetail) {
+    return {
+      title: '프로젝트를 찾을 수 없습니다',
+      description: '요청한 프로젝트를 찾을 수 없습니다.',
+      icons: {
+        icon: '/default-icon.ico'
+      }
+    };
+  }
+
+  return {
+    title: projectDetail.head?.title,
+    description: projectDetail.head?.shortDescription,
+    icons: {
+      icon: projectDetail.head?.thumbnailImageUrl
+    },
+    openGraph: {
+      title: projectDetail.head?.title,
+      description: projectDetail.head?.shortDescription,
+      type: 'website',
+      url: `https://si-tree.com/projects/${params.projectId}`,
+      site_name: 'Si-Tree',
+      locale: 'ko_KR',
+      images: [
+        {
+          url: projectDetail.head?.thumbnailImageUrl,
+          width: 800,
+          height: 600,
+          alt: projectDetail.head?.title
+        },
+        ...projectDetail.overview.images.map((image) => ({
+          url: image.imageUrl,
+          width: 800,
+          height: 600,
+          alt: projectDetail.head?.title
+        }))
+      ]
+    }
   };
 }
 
