753e8aa502e50b7faa6693519ab94bb143ffa900
shunn2 <chsida655@gmail.com>
2025-03-14 19:44:13 +0900

    feat: GNB auth context 적용

--- src/components/account/SignInButton.tsx
+++ src/components/account/SignInButton.tsx
@@ -8,8 +8,11 @@ type Props = {
 };
 
 const SignInButton = ({ authType, className }: Props) => {
+  const currentPath = window.location.pathname;
+
   const handleSignIn = () => {
-    signIn(authType, { callbackUrl: '/onboarding' });
+    console.log('currentPath', currentPath);
+    signIn(authType, { callbackUrl: currentPath });
   };
 
   return (
