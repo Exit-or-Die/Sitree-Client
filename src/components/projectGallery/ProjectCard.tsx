7af0d856b025ded9c577b26f3028086874c5d8eb
yunko <yk1803@nyu.edu>
2025-04-07 15:11:04 +0900

    chore: fix project card

--- src/components/projectGallery/ProjectCard.tsx
+++ src/components/projectGallery/ProjectCard.tsx
@@ -96,17 +96,11 @@ const ProjectCard = ({
           <div className="flex text-xsmall text-gray-400 mt-4 pl-2 justify-between">
             <div className="flex">
               <div className="flex items-center mr-2">
-                <SImage
-                  src="/comment.svg"
-                  width={12}
-                  height={12}
-                  alt="comment"
-                  className="mr-[3px]"
-                />{' '}
+                <SvgIcon icon="comment" color="#778195" className="mr-[3px]" width={12} height={12} />{' '}
                 <span className="text-slate-30">{commentCount}</span>
               </div>
               <div className="flex items-center mr-2">
-                <SImage src="/like.svg" width={12} height={12} alt="like" className="mr-[3px]" />
+                <SvgIcon icon="like" color="#778195" className="mr-[3px]" width={12} height={12} />{' '}
                 <span className="text-slate-30">{likesCount}</span>
               </div>
               <span className="text-slate-50">조회수 {viewCount}</span>
